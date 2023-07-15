1dc7eb761d6b6dbfbd660c843f5465180a39cabb
Juan Pablo Tuttolomondo <jptuttolomondo@hotmail.com>
2023-07-15 15:37:17 -0300

    'ulttimo'

--- front/front-upskilling/vite.config.js
+++ front/front-upskilling/vite.config.js
@@ -1,7 +1,23 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    strictPort: true,
+    port: 5173,
+    watch: {
+      usePolling: true
+    }
+  }
 })
